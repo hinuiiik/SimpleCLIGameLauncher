e6125d43e17d9636218954acd60e0c7b61a20fbd
hinuiiik <vknc@protonmail.com>
2023-01-04 14:14:47 -0500

    start gamelb

---
+++ SimpleGameLauncher/Game.cs
@@ -0,0 +1,22 @@
+namespace SimpleGameLauncher;
+
+public class Game
+{
+    private string _GameAddress;
+    private string _GameName;
+    private int _GameID;
+
+    public Game(string gameAddress, string gameName, int gameId)
+    {
+        this._GameAddress = gameAddress;
+        this._GameName = gameName;
+        this._GameID = gameId;
+    }
+    
+    
+    
+    
+    
+    
+    
+}
